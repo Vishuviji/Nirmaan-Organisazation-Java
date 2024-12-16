4e1578151ec3698be61092b10928d245190ff975
Vishuviji <113893829+Vishuviji@users.noreply.github.com>
2024-12-16 16:51:00 +0530

    Day 2

---
+++ VariableTask.java
@@ -0,0 +1,28 @@
+class VariableTask{
+    public static void main(String[]args){
+
+byte num1=114;
+short num2=1567;
+int num3= 123456;
+long num4=123456789L;
+float num5= 3.14f;
+double num6= 3.1456d;
+String name="Vishnu";
+char a='2';
+boolean isPresent= true;
+
+System.out.println("byte :"+num1);
+System.out.println("short :"+num2);
+System.out.println("int :"+num3);
+System.out.println("long :"+num4);
+System.out.println("float :"+num5);
+System.out.println("double :"+num6);
+System.out.println("String :"+name);
+System.out.println("char :"+a);
+System.out.println("boolean : "+isPresent);
+
+
+
+}
+
+}
