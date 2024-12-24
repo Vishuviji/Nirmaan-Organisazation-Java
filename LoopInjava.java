cded31e53f56849b2b05626cd948fa6aba3ed821
Vishuviji <113893829+Vishuviji@users.noreply.github.com>
2024-12-24 15:53:27 +0530

    day4

---
+++ LoopInjava.java
@@ -0,0 +1,60 @@
+package day4;
+import java.util.Scanner;
+public class LoopInjava {
+
+ public static void main(String []args) {
+
+ /* for (int i=0;i<=100;i++) {
+
+	 if(i%5==0) {
+	  System.out.println("vishnu");
+	 }
+	 else {
+		  System.out.println("i");
+		 }*/
+	 
+	 /*Scanner sc= new Scanner(System.in);
+System.out.println("Enter the table");
+
+int n=sc.nextInt();
+for (int j=0;j<=10;j++) {
+System.out.println(n+"x"+j+"=" +j*n);
+}*/
+	 
+	/* for (int i=1;i<=10;i++) {
+
+		 if(i==10) {
+	continue;
+		 }
+		 else {
+			  System.out.println(i);
+			 }
+	 }
+	*/
+	 for (int i=1;i<=10;i++) {
+
+		 if(i==10) {
+			 
+	        break;
+		 }
+		 else {
+			  System.out.println(i);
+			 }
+	 }
+	/*  for (int i=1;i<=4;i++) {	 
+		  for (int j=1;j<=4;j++) {
+	      System.out.print("* ");
+}
+		  {
+			  System.out.println();
+		  }
+	  }
+	  */
+	 /*for (int i=1;i<=4;i++) {	 
+		  for (int j=1;j<=4;j++) {
+	      System.out.print(j);
+}
+		  
+			  System.out.println();
+		  }*/
+	  }}
