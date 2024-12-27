a344ca9fb87d9d0ace35c6644ae3296b1b779ac2
Vishuviji <113893829+Vishuviji@users.noreply.github.com>
2024-12-27 16:04:30 +0530

    Day 6

---
+++ Taskdatatype.java
@@ -0,0 +1,30 @@
+package day5;
+
+public class Taskdatatype {
+	 public static void main(String []args) {
+
+		/*String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is
+				 going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were
+				 protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of
+				 the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned,
+				 "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped
+				 "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year
+				 "+number+", in a world where "+noun6+"s ruled the world."	 */
+		 String name1 = "Liam";
+		 String adjective1 ="sleepy";
+		 String name2 = "maya";
+		 String adjective2= "stressful";
+		 String noun1 = "kittens";
+		 String noun2 = "pancakes";
+		 String verb1 = "sing";
+		 String noun3 = "bongos";
+		 String noun4 = "frogs";
+		 String adjective3 ="shocked";
+		 String place1 ="moon";
+		 String noun5 = "slush";
+		 String noun6 = "unicorns";
+		 int number= 1900;
+		 
+		 System.out.println("This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s wereprotesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm ofthe "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped"+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year"+number+", in a world where "+noun6+"s ruled the world.");
+		 
+}}
