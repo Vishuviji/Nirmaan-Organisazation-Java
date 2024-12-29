1b51b91733a34a0661fffb242f54b2d0835cfb82
Vishuviji <113893829+Vishuviji@users.noreply.github.com>
2024-12-29 18:40:52 +0530

    day 6-task 2

---
+++ Reverse.java
@@ -0,0 +1,39 @@
+public class Reverse {
+
+ public static void main(String []args) {
+
+	 // Reserve
+
+  /* for (int i=50; i>=0; i--)
+
+ {
+
+  System.out.println(i);
+
+ }
+
+ */
+	 for (int i=1; i<=100; i++)
+
+	 {
+
+	
+
+	if(i%2!=0) {
+
+		
+  System.out.println("the sum of odd number is :"+i);
+	}
+
+	 }
+
+	 
+/*
+for(int i=1;i<=30;i++)
+{
+if(i%3==0)
+System.out.print(i+",");
+}
+*/
+}
+}
