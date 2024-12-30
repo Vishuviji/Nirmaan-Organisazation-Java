20c6ef1526e09c362ed30022a91fea8a41cdaf22
Vishuviji <113893829+Vishuviji@users.noreply.github.com>
2024-12-30 17:11:23 +0530

    day7

---
+++ TypeConvert.java
@@ -0,0 +1,38 @@
+package day7;
+
+public class TypeConvert {
+
+	public static void main(String []args) {
+
+	/*	String name="vishnu";
+		String rs="";
+		for(int i=5;i>=0;i--)
+			rs+=name.charAt(i);
+		{
+			
+	System.out.println(rs.toLowerCase());
+	System.out.println(rs.toUpperCase());
+	}*/
+		
+		// Typecasting
+		
+		//implicit casting
+		
+	/*	byte b=127;
+		int i=b;
+		System.out.println(b);
+		*/
+		
+		//Explicit casting
+/*	int i=129;
+	byte b=(byte)i;
+		System.out.println(b);*/
+		
+		//Array
+		String[] veg= {"onion","tomato","potato"};
+		for(int i=0;i<veg.length;i++)
+	
+			System.out.println(veg[i]);
+		
+	}
+}
