950563f66f2b4435c6560dc14f88e4697e6dc3df
Vishuviji <113893829+Vishuviji@users.noreply.github.com>
2025-02-05 16:17:51 +0530

    Day 20-task2

---
+++ wrapper.java
@@ -0,0 +1,47 @@
+package wrapperex;
+
+
+
+public class wrapper {
+public static void main(String[] args) {
+	int num=60;
+	Integer num1=num;
+	int num2=num1;
+	System.out.println(num1);
+	
+	byte a=127;
+	Byte a1=a;
+	byte a2=a1;
+	System.out.println(a1);
+	
+	float b=8;
+	Float b1=b;
+	float b2=b1;
+	System.out.println(b1);
+	
+	char c='v';
+	Character c1=c;
+	char c2=c1;
+	System.out.println(c1);
+	
+	short d=123;
+	Short d1=d;
+	short d2=d1;
+	System.out.println(d1);
+	
+	long e=8925210367l;
+	Long e1=e;
+	long e2=e1;
+	System.out.println(e1);
+		
+	double f=2345;
+	Double f1=f;
+	double f2=f1;
+	System.out.println(f1);
+	
+	
+	
+			
+	
+}
+}
